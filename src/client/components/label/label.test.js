885928cf062007fd08434b5fe517760a279a5f2b
Zaki Wasik <zakiwa@gmail.com>
2020-03-13 00:02:51 +0100

    fix: correct test

--- src/client/components/label/label.test.js
+++ src/client/components/label/label.test.js
@@ -3,22 +3,10 @@ import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Label from './label.component';
 
-test('Label has class active when active prop is true', () => {
-  expect(
-    render(<Label active={true} title="test title" />).container.firstChild,
-  ).toHaveClass('active');
-});
-
-test('Label does not have class active when active prop is false', () => {
-  expect(
-    render(<Label active={false} title="test title" />).container.firstChild,
-  ).not.toHaveClass('active');
-});
-
 test('Label should contain title', () => {
   const testTitle = 'abc';
   expect(
-    render(<Label active={true} title={testTitle} />).container.firstChild
-      .textContent,
+    render(<Label backgroundColor="#000" title={testTitle} />).container
+      .firstChild.textContent,
   ).toBe(testTitle);
 });
