e7430effe38c96e3057df9348a3aa086ac62bcfa
amjadiftikhar <amjadiftikhar@github.com>
2020-03-13 16:27:33 +0100

    stories edited

---
+++ src/client/components/ProgressBar/ProgressBar.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ProgressBar from './ProgressBar.component';
+
+export default { title: 'Progress bar' };
+
+export const progressBar = () => <ProgressBar />;
--- src/client/components/ProgressBar/ProgressBar.story.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import ProgressBar from './ProgressBar.component';
-
-storiesOf('ProgressBar', module).add('progress bar', () => <ProgressBar />);
