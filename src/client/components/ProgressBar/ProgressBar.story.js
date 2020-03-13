f8ff19ab94a2c8c41ea035eb2f939b0bab3b333f
humayunadilshahzad <humayun_adil@hotmail.com>
2020-03-13 01:04:31 +0100

    fixed tool tip added showing day number

--- src/client/components/ProgressBar/ProgressBar.story.js
+++ src/client/components/ProgressBar/ProgressBar.story.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import ProgressBar from './Progressbar.componenet';
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import ProgressBar from "./Progressbar.componenet";
 
-storiesOf('Progress Bar', module).add('Crop Progress Bar', () => (
+storiesOf("Progress Bar", module).add("Crop Progress Bar", () => (
   <ProgressBar />
 ));
--- src/client/components/ProgressBar/Progressbar.componenet.js
+++ src/client/components/ProgressBar/Progressbar.componenet.js
@@ -1,18 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './ProgressBar.style.css';
 
 export default function ProgressBar() {
   const CropTotalDays = 90;
-  const CropDaysPassed = 20;
+  const CropDaysPassed = 50;
+  let show = "hidden";
   const arr = [];
-  const [days, setDays] = useState(CropTotalDays);
-  for (let i = 0; i < days; i += 1) arr.push(i);
+  // const [days, setDays] = useState(CropTotalDays);
+  for (let i = 0; i < CropTotalDays; i += 1) arr.push(i);
 
   return (
     <>
       <h4>Progress Bar</h4>
       <div className="progressbar">
         {arr.map((e) => {
+          
+            if (e + 1 === CropDaysPassed) show = "visible"
+            else show = "hidden";
+          
           if (e < CropDaysPassed) {
             return (
               <div
@@ -21,24 +26,23 @@ export default function ProgressBar() {
                 className="compmid tooltip"
                 style={{ width: 1000 / arr.length }}
               >
-                  <span class="tooltiptext">day:{e+1}</span>
-
+                <span className="tooltiptext" style={{ visibility: show }}>
+                  Day:{e + 1}
+                </span>
               </div>
-              
-            )
-          } else {
-            return (
-              <div
-                type="text"
-                key={e}
-                className="compmid"
-                style={{
-                  width: 1000 / arr.length,
-                  backgroundColor: 'lightgrey',
-                }}
-              ></div>
             );
           }
+          return (
+            <div
+              type="text"
+              key={e}
+              className="compmid"
+              style={{
+                width: 1000 / arr.length,
+                backgroundColor: "lightgrey"
+              }}
+            />
+          );
         })}
       </div>
     </>
