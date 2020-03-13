eebb2fd771e1963540da0a9b7a56c668ac687daa
Kristina Fazinić <kfazinic@outlook.com>
2020-03-13 20:03:19 +0100

    Rewrote StageDetails story

--- src/client/components/stage-details/stage-details.stories.js
+++ src/client/components/stage-details/stage-details.stories.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { withKnobs } from '@storybook/addon-knobs';
 import StageDetails from './stage-details.component';
-import { storiesOf } from '@storybook/react';
 
-storiesOf('Accordion Options', module).add('Stage Details', () => {
-  const stageDetailsNameTitlePairs = {
-    temperature: 'Temperature (°C)',
-    humidity: 'Humidity (g/m3)',
-    ph: 'PH',
-    ec: 'EC (ppm)',
-    'water-level': 'Water level (cm)',
-  };
+export default {
+  component: StageDetails,
+  title: 'Accordion Options',
+  decorators: [withKnobs],
+};
 
+const stageDetailsNameTitlePairs = {
+  temperature: 'Temperature (°C)',
+  humidity: 'Humidity (g/m3)',
+  ph: 'PH',
+  ec: 'EC (ppm)',
+  'water-level': 'Water level (cm)',
+};
+
+export const StageDetailsComponent = () => {
   const stages = ['Seeding', 'Propagation', 'Maturity', 'Harvest'];
   const levels = ['Optimum', 'Min', 'Max'];
 
@@ -19,7 +26,7 @@ storiesOf('Accordion Options', module).add('Stage Details', () => {
       inputNameTitlePairsObj={stageDetailsNameTitlePairs}
       buttonValuesArr={stages}
       labelValuesArr={levels}
-      saveFunc={() => null}
+      saveFunc={action('save stage details')}
     />
   );
-});
+};
