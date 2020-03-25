5ce902caf49ba6006e40601b8345f7444615f0a0
amjadiftikhar <amjadiftikhar@github.com>
2020-03-25 23:04:06 +0100

    crops, crop_stages, units tables been added

--- src/server/migrations/20200325162239_crops.js
+++ src/server/migrations/20200325162239_crops.js
@@ -1,15 +1,21 @@
-exports.up = function (knex) {
-  return knex.schema.createTable('crops', function (table) {
+exports.up = function(knex) {
+  return knex.schema.createTable('crops', function(table) {
     table.increments();
     table.varchar('name', 255).notNullable();
     table.varchar('plant_variety', 255).notNullable();
-    table.integer('fk_user_id').unsigned().notNullable();
+    table
+      .integer('fk_user_id')
+      .unsigned()
+      .notNullable();
     table.timestamp('created_at').notNullable();
     table.timestamp('updated_at').notNullable();
     table.timestamp('deleted_at').notNullable();
-    table.foreign('fk_user_id').references('id').inTable('users');
+    table
+      .foreign('fk_user_id')
+      .references('id')
+      .inTable('users');
   });
 };
-exports.down = function (knex) {
+exports.down = function(knex) {
   return knex.schema.dropTableIfExists('crops');
 };
