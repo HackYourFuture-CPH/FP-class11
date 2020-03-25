247b5303bd8ed2d190e02bb2109a75a457fa3d05
mahie akhtar <mahie.akhtar@outlook.com>
2020-03-25 13:32:56 +0100

    folder and filename changed to newname

---
+++ src/client/components/Crop1/AddCrop1/addcrop.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import AddCrop from './addcrop';
+
+export default {
+  title: 'AddCrop',
+};
+
+export const AddCropStories = () => <AddCrop />;
---
+++ src/client/components/Crop1/Input1/InputText.stories.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import InputText from './inputtext';
+
+export default {
+  title: 'InputText',
+};
+export const InputField = () => <InputText placeholder="text" />;
