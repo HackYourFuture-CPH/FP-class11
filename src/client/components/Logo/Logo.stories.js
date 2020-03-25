f9414ff2964597da571ff8f9f77c44edc57286e8
mamathamereddy <mamathavangala1@gmail.com>
2020-03-25 14:25:04 +0100

    refactoring logo

--- src/client/components/Logo/Logo.stories.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Logo from './logo.component';
-import Seasony from './seasony.jpg';
-
-export default { title: 'logo', component: 'Logo' };
-
-export const logoimg = () => <Logo src={Seasony} alt="Seasony-img" />;
