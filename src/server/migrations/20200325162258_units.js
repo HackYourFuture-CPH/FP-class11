e64d7c8d179cdb61c8b8f6aa82196e34e9129d1d
amjadiftikhar <amjadiftikhar@github.com>
2020-03-25 22:46:54 +0100

    crops, crop_stages, units table have been updated

---
+++ src/server/migrations/20200325162258_units.js
@@ -0,0 +1,23 @@
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('crop_stages');
+  };
+  
+  exports.up = function(knex) {
+    return knex.schema.createTable('units', function(table) {
+      table
+        .increments('id')
+        .primary()
+        .unsigned()
+        .notNullable();
+      table
+        .varchar('name')
+        .unique()
+        .notNullable();
+      table.timestamp('created_at').notNullable();
+      table.timestamp('updated_at').notNullable();
+      table.timestamp('deleted_at').notNullable();
+    });
+  };
+  exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('units');
+  };
---
+++ src/server/migrations/20200325162313_crop_stages.js
@@ -0,0 +1,14 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('crop_stages', function(table) {
+      table
+        .increments('id')
+        .primary()
+        .unsigned();
+      table.enu('name', []).notNullable();
+      table.timestamp('created_at').notNullable();
+      table.timestamp('updated_at').notNullable();
+    });
+  };
+  exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('crop_stages');
+  };
