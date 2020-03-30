7b37149466a76e0df954d369f7e93479b47b41c3
zenkaeser <b7acksoul@gmail.com>
2020-03-31 00:38:27 +0200

    added endpoints for crops

--- src/server/api/routes/api-router.js
+++ src/server/api/routes/api-router.js
@@ -5,7 +5,7 @@ const router = express.Router();
 // Router imports
 const modulesRouter = require('./modules.router');
 const cropStageDefaultValuesRouter = require('./crop-stage-default-values.router');
-const cropsRouter = require("./crops.router");
+const cropsRouter = require('./crops.router');
 
 // swagger-ui-express
 const swaggerDocument = require('../../config/swagger.json');
@@ -17,5 +17,6 @@ router.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // Application routes
 router.use('/modules', modulesRouter);
 router.use('/crop-stage-parameter-values', cropStageDefaultValuesRouter);
+router.use('/crops', cropsRouter);
 
 module.exports = router;
--- src/server/api/routes/crops.router.js
+++ src/server/api/routes/crops.router.js
@@ -1,16 +1,16 @@
 /* eslint-disable no-console */
-const express = require("express");
+const express = require('express');
 
 const router = express.Router();
 
 // controllers
-const cropsController = require("../controllers/crops.controller");
+const cropsController = require('../controllers/crops.controller');
 
 // ENDPOINT: /api/crops/ :GET to get all crops
-router.get("/", (req, res, next) => {
+router.get('/', (req, res, next) => {
   cropsController
     .getAllCrops()
-    .then(result => res.json(result))
+    .then((result) => res.json(result))
     .catch(next);
 });
 
