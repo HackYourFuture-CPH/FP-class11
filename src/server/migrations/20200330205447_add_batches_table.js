616a0f494f5438161fe75ff57dfed7ad6f2a1a00
Sreelatha Vallabhaneni <sreelatha.vallabhaneni@gmail.com>
2020-03-30 21:01:44 +0200

    recreated batches table based on new DB

---
+++ src/server/migrations/20200330205447_add_batches_table.js
@@ -0,0 +1,42 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('batches', function(table) {
+    table
+      .increments('id')
+      .primary()
+      .notNullable();
+    table
+      .integer('fk_crop_id')
+      .notNullable()
+      .unsigned();
+    table
+      .integer('fk_user_id')
+      .notNullable()
+      .unsigned();
+    table.string('customer_name').notNullable();
+    table
+      .timestamp('seeding_date')
+      .defaultTo(knex.fn.now())
+      .notNullable();
+    table
+      .timestamp('created_at')
+      .defaultTo(knex.fn.now())
+      .notNullable();
+    table
+      .timestamp('updated_at')
+      .defaultTo(knex.fn.now())
+      .notNullable();
+    table.timestamp('deleted_at');
+    table
+      .foreign('fk_crop_id')
+      .references('id')
+      .inTable('crops');
+    table
+      .foreign('fk_user_id')
+      .references('id')
+      .inTable('users');
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('batches');
+};
