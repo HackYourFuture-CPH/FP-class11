4be1fb6cff0ff688c51a2ac679b226fb0a2be59d
MirSzabo <poradova.m@gmail.com>
2020-03-31 22:09:22 +0200

    props added

--- src/client/components/detail-chart/detail-chart.component.js
+++ src/client/components/detail-chart/detail-chart.component.js
@@ -1,3 +1,5 @@
+/* eslint-disable camelcase */
+/* eslint-disable @typescript-eslint/camelcase */
 import React from 'react';
 import './detail-chart.style.css';
 import PropTypes from 'prop-types';
@@ -13,7 +15,22 @@ import {
   ReferenceLine,
 } from 'recharts';
 
-export default function DetailChart({ data }) {
+export default function DetailChart({
+  data,
+  materialId,
+  boundary,
+  description,
+  unit,
+}) {
+  const { sensors_readings } = data;
+
+  const filterByMaterialId = sensors_readings.filter(
+    (sensor) => sensor.fk_material_id === materialId.temperature,
+  );
+
+  const formatXAxis = () =>
+    filterByMaterialId.map((sensor) => sensor.created_at.slice(0, 13));
+
   return (
     <div className="App">
       <ResponsiveContainer
@@ -21,24 +38,24 @@ export default function DetailChart({ data }) {
         width="100%"
         height={400}
       >
-        <LineChart data={data} margin={{ right: 80 }}>
+        <LineChart data={filterByMaterialId} margin={{ right: 80 }}>
           <XAxis
             stroke="#DDE0E3"
-            dataKey="timestamp"
+            dataKey={formatXAxis}
+            name="Time"
             height={50}
             domain={['dataMin', 'dataMax']}
             tick={{
-              transform: 'translate(-20, 0)',
               fontFamily: 'Roboto',
               textAlign: 'right',
-              fontSize: 10,
+              fontSize: 8,
               fill: '#A3A3A3',
             }}
           />
           <YAxis
             stroke="#DDE0E3"
             type="number"
-            domain={['dataMin', 'dataMax']}
+            domain={[boundary.minimum - 3, boundary.maximum + 3]}
             tick={{
               transform: 'translate(-20, 0)',
               fontFamily: 'Open Sans',
@@ -46,29 +63,38 @@ export default function DetailChart({ data }) {
               fill: '#666666',
             }}
             fill="none"
-            ticks={[10, 15, 20, 25, 30]}
           />
           <Tooltip />
-          <ReferenceLine y={20} stroke="#6F9C67" strokeWidth={2} />
-          <ReferenceLine y={15} stroke="#9C5256" strokeWidth={2} />
-          <ReferenceLine y={25} stroke="#9C5256" strokeWidth={2} />
+          <ReferenceLine
+            y={boundary.optimum}
+            stroke="#6F9C67"
+            strokeWidth={2}
+          />
+          <ReferenceLine
+            y={boundary.maximum}
+            stroke="#9C5256"
+            strokeWidth={2}
+          />
+          <ReferenceLine
+            y={boundary.minimum}
+            stroke="#9C5256"
+            strokeWidth={2}
+          />
           <Line
             type="monotone"
-            name="Temperature"
-            unit="°C"
-            dataKey="temp"
+            name={description.temperature}
+            unit={unit.temperature}
+            dataKey="reading_value"
             stroke="#000000"
             fill="#000000"
             strokeWidth={2}
           />
-
           <Brush
             className="range-bar"
             startIndex={0}
-            endIndex={50}
+            endIndex={10}
             stroke="#73ABD7"
-            width="100%"
-            height={20}
+            height={30}
             travellerWidth={15}
             gap={5}
           />
@@ -78,6 +104,14 @@ export default function DetailChart({ data }) {
   );
 }
 
+DetailChart.defaultProps = {
+  materialId: '1',
+};
+
 DetailChart.propTypes = {
-  data: PropTypes.string.isRequired,
+  data: PropTypes.oneOfType([PropTypes.object]).isRequired,
+  materialId: PropTypes.string,
+  boundary: PropTypes.number.isRequired,
+  description: PropTypes.string.isRequired,
+  unit: PropTypes.string.isRequired,
 };
--- src/client/components/detail-chart/detail-chart.stories.js
+++ src/client/components/detail-chart/detail-chart.stories.js
@@ -1,7 +1,47 @@
 import React from 'react';
 import DetailChart from './detail-chart.component';
-import data from './lettuce.json';
+import data from './sensor_readings.json';
 
-export default { title: 'Charts/Detail Chart' };
+export default {
+  title: 'Charts/Detail Chart',
+};
 
-export const withRangeBar = () => <DetailChart data={data} />;
+const materialId = {
+  temperature: '1',
+  humidity: '2',
+  PH: '3',
+  EC: '4',
+  water: '5',
+};
+
+const boundary = {
+  optimum: 23,
+  minimum: 16,
+  maximum: 28,
+};
+
+const description = {
+  temperature: 'Temperature',
+  humidity: 'Humidity',
+  PH: 'PH',
+  EC: 'EC',
+  water: 'Water level',
+};
+
+const unit = {
+  temperature: '°C',
+  humidity: 'g/m3',
+  PH: '',
+  EC: 'ppm',
+  water: 'cm',
+};
+
+export const withRangeBar = () => (
+  <DetailChart
+    data={data}
+    materialId={materialId}
+    boundary={boundary}
+    description={description}
+    unit={unit}
+  />
+);
