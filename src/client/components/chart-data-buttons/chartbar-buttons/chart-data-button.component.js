b47885a5e897da6a20dc096e8af1dade72fa6352
mamathamereddy <mamathavangala1@gmail.com>
2020-03-31 15:37:18 +0200

    modified the component with state and working fine

--- src/client/components/chart-data-buttons/chartbar-buttons/chart-data-button.component.js
+++ src/client/components/chart-data-buttons/chartbar-buttons/chart-data-button.component.js
@@ -1,33 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './chart-data-button.style.css';
 import PropTypes from 'prop-types';
 import Button from '../../button/button.component';
 
-const ChartbarMenu = ({ text, onClick }) => {
+const ChartbarMenu = ({ buttonLabel }) => {
+  const [selectedButton, setSelectedButton] = useState(buttonLabel.buttonWeek);
   return (
     <div className="chartbar-button-wrapper">
-      <Button type="toggle" size="large" onClick={onClick}>
-        {text.buttonDays}
+      <Button
+        type="toggle"
+        size="large"
+        onClick={(e) => {
+          e.preventDefault();
+          setSelectedButton(buttonLabel.buttonDays);
+        }}
+        toggled={selectedButton === buttonLabel.buttonDays}
+      >
+        {buttonLabel.buttonDays}
       </Button>
-      <Button type="toggle" size="large" onClick={onClick}>
-        {text.buttonWeek}
+      <Button
+        type="toggle"
+        size="large"
+        onClick={(e) => {
+          e.preventDefault();
+          setSelectedButton(buttonLabel.buttonWeek);
+        }}
+        toggled={selectedButton === buttonLabel.buttonWeek}
+      >
+        {buttonLabel.buttonWeek}
       </Button>
-      <Button type="toggle" size="large" onClick={onClick}>
-        {text.buttondays}
+      <Button
+        type="toggle"
+        size="large"
+        onClick={(e) => {
+          e.preventDefault();
+          setSelectedButton(buttonLabel.buttonWeeks);
+        }}
+        toggled={selectedButton === buttonLabel.buttonWeeks}
+      >
+        {buttonLabel.buttonWeeks}
       </Button>
-      <Button type="toggle" size="large" onClick={onClick}>
-        {text.buttonMonth}
+      <Button
+        type="toggle"
+        size="large"
+        onClick={(e) => {
+          e.preventDefault();
+          setSelectedButton(buttonLabel.buttonMonth);
+        }}
+        toggled={selectedButton === buttonLabel.buttonMonth}
+      >
+        {buttonLabel.buttonMonth}
       </Button>
-      <Button type="toggle" size="large" onClick={onClick}>
-        {text.buttonCustom}
+      <Button
+        type="toggle"
+        size="large"
+        onClick={(e) => {
+          e.preventDefault();
+          setSelectedButton(buttonLabel.buttonCustom);
+        }}
+        toggled={selectedButton === buttonLabel.buttonCustom}
+      >
+        {buttonLabel.buttonCustom}
       </Button>
     </div>
   );
 };
 
 ChartbarMenu.propTypes = {
-  text: PropTypes.string.isRequired,
-  onClick: PropTypes.func.isRequired,
+  buttonLabel: PropTypes.oneOfType([PropTypes.object]).isRequired,
 };
 
 export default ChartbarMenu;
--- src/client/components/chart-data-buttons/chartbar-buttons/chart-data-button.stories.js
+++ src/client/components/chart-data-buttons/chartbar-buttons/chart-data-button.stories.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ChartbarMenu from './chart-data-button.component';
 import { withKnobs, object } from '@storybook/addon-knobs';
-import { action } from '@storybook/addon-actions';
 
 export default {
   title: 'ChartDataButtons',
@@ -13,12 +12,12 @@ const label = 'value';
 const defaultValue = {
   buttonDays: 'Last 5 Days',
   buttonWeek: 'Last Week',
-  buttondays: 'Last Two Weeks',
+  buttonWeeks: 'Last Two Weeks',
   buttonMonth: 'Last Month',
   buttonCustom: 'Custom',
 };
 
 export const Chartdatabuttons = () => {
   const value = object(label, defaultValue);
-  return <ChartbarMenu text={value} onClick={action('clicked')} />;
+  return <ChartbarMenu buttonLabel={value} />;
 };
