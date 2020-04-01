ccbbdf22dd9813901aee5e8ec8bcebdbb86da6c6
humayunadilshahzad <humayun_adil@hotmail.com>
2020-04-01 23:06:41 +0200

    migrations with seeds updated

--- src/server/migrations/20200325162239_crops.js
+++ src/server/migrations/20200325162239_crops.js
@@ -1,24 +1,14 @@
-exports.up = function(knex) {
-  return knex.schema.createTable('crops', function(table) {
+exports.up = function (knex) {
+  return knex.schema.createTable('crops', function (table) {
     table.increments();
     table.varchar('name', 255).notNullable();
     table.varchar('plant_variety', 255).notNullable();
-    table
-      .integer('fk_user_id')
-      .unsigned()
-      .notNullable();
-    table
-      .timestamp('created_at')
-      .defaultTo(knex.fn.now())
-      .notNullable();
-    table
-      .timestamp('updated_at')
-      .defaultTo(knex.fn.now())
-      .notNullable();
-    table
-      .timestamp('deleted_at');
+    table.integer('fk_user_id').unsigned().notNullable();
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable();
+    table.timestamp('updated_at').defaultTo(knex.fn.now()).notNullable();
+    table.timestamp('deleted_at');
   });
 };
-exports.down = function(knex) {
+exports.down = function (knex) {
   return knex.schema.dropTableIfExists('crops');
 };
--- src/server/seeds/03_crop_stages.js
+++ src/server/seeds/03_crop_stages.js
@@ -1,15 +1,15 @@
-
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('crop_stages').del()
+  return knex('crop_stages')
+    .del()
     .then(function () {
       // Inserts seed entries
       return knex('crop_stages').insert([
-        {name: 'seeding',duration:'3',fk_crops_id:'251'},
-        {name: 'propagation',duration:'14',fk_crops_id:'251'},
-        {name: 'meturity',duration:'28',fk_crops_id:'251'},
-        {name: 'harvesting',duration:'3',fk_crops_id:'251'},
-        {name: 'delivery',duration:'4',fk_crops_id:'251'}
+        { name: 'seeding', duration: '3', fk_crops_id: '251' },
+        { name: 'propagation', duration: '14', fk_crops_id: '251' },
+        { name: 'meturity', duration: '28', fk_crops_id: '251' },
+        { name: 'harvesting', duration: '3', fk_crops_id: '251' },
+        { name: 'delivery', duration: '4', fk_crops_id: '251' },
       ]);
     });
 };
