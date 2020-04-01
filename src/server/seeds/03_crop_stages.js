be0c19ccca3ff8e2fbe7c24a63fb11c5fb153832
humayunadilshahzad <humayun_adil@hotmail.com>
2020-04-02 01:03:13 +0200

    crops and crop_stages updated

--- src/server/seeds/03_crop_stages.js
+++ src/server/seeds/03_crop_stages.js
@@ -5,11 +5,11 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex('crop_stages').insert([
-        { name: 'seeding', duration: '3', fk_crops_id: '251' },
-        { name: 'propagation', duration: '14', fk_crops_id: '251' },
-        { name: 'meturity', duration: '28', fk_crops_id: '251' },
-        { name: 'harvesting', duration: '3', fk_crops_id: '251' },
-        { name: 'delivery', duration: '4', fk_crops_id: '251' },
+        { name: 'seeding', duration: '3', fk_crops_id: '51' },
+        { name: 'propagation', duration: '14', fk_crops_id: '51' },
+        { name: 'meturity', duration: '28', fk_crops_id: '51' },
+        { name: 'harvesting', duration: '3', fk_crops_id: '51' },
+        { name: 'delivery', duration: '4', fk_crops_id: '51' },
       ]);
     });
 };
