ae544a51b9eac15315e530605b3998c81365bfb1
Svetlana Stosic Milosavljevic <cecastosic@gmail.com>
2020-04-03 00:50:20 +0200

    removing toString

--- src/server/api/controllers/users.controller.js
+++ src/server/api/controllers/users.controller.js
@@ -17,7 +17,7 @@ const getRole = async (uid) => {
       .join('user_roles', 'user_roles.fk_role_id', '=', 'roles.id')
       .join('users', 'users.id', '=', 'user_roles.fk_user_id')
       .select('roles.name')
-      .where('users.uid', uid.toString());
+      .where('users.uid', uid);
     if (role.length === 0) {
       throw new Error(`incorrect entry with the uid of ${uid}`, 404);
     }
