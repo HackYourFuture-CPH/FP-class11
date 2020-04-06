d092aa8e985bafc0e516893802de722be2b7194e
Gizem Candemir <gizemc@gmail.com>
2020-04-06 16:46:45 +0200

    Rename pages from crop to batch

---
+++ src/client/containers/add-batch-page/add-batch-page.component.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import SidebarMenu from '../../components/side-navigation/sidebar.component';
+import AddBatch from '../../components/batch/add-batch.component';
+import AddBatchImage from '../../components/batch/add-batch-image.component';
+import Footer from '../../components/footer/footer.component';
+
+const AddBatchPage = () => {
+  return (
+    <>
+      <div>
+        <SidebarMenu isActive={true} isUser={true} />
+      </div>
+      <div>
+        <AddBatch />
+        <AddBatchImage />
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default AddBatchPage;
