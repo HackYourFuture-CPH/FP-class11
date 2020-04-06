800909c86d88a6741e2dbf23bb160884a849a431
humayunadilshahzad <humayun_adil@hotmail.com>
2020-04-06 22:13:43 +0200

    crop stages end point updated

--- src/server/api/controllers/modules.controller.crop-stages-endpoint.js
+++ src/server/api/controllers/modules.controller.crop-stages-endpoint.js
@@ -2,17 +2,18 @@ const knex = require('../../config/db');
 
 const getCropStages = async () => {
   try {
-    const startDate = await knex('batches')
-      .select('seeding_date')
-      .where({ id: '31' });
+    const startDate = await knex('batches').select('seeding_date');
     const stages = await knex('crop_stages').select(
       'crop_stages.name',
       'crop_stages.duration',
     );
-    const date = new Date();
+    if (startDate.length === 0 || stages.length === 0) {
+      throw new Error(`Batch or stages not found `, 404);
+    }
+    const currentDate = new Date().toLocaleString();
     return {
-      startDate: startDate[0].seeding_date,
-      date,
+      startDate: new Date(startDate[0].seeding_date).toLocaleString(),
+      currentDate,
       stages,
     };
   } catch (error) {
--- src/server/api/routes/modules.router.js
+++ src/server/api/routes/modules.router.js
@@ -18,7 +18,7 @@ router.get('/', (req, res, next) => {
 // ENDPOINT: /api/modules/:id :GET to get one module
 router.get('/:id', (req, res, next) => {
   modulesController
-    .getModuleById(req.params.id)
+    .getModules(req.params.id)
     .then((result) => res.json(result))
     .catch(next);
 });
