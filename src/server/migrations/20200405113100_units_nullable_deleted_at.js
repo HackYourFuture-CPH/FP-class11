350442e4ce256ff2100dfa6af3e87f06e3ccf2fc
Zaki Wasik <zakiwa@gmail.com>
2020-04-06 18:11:00 +0200

    feat: add migration to fix crop stages name enum

--- src/server/migrations/20200405113100_units_nullable_deleted_at.js
+++ src/server/migrations/20200405113100_units_nullable_deleted_at.js
@@ -8,12 +8,10 @@ exports.up = function(knex) {
 };
 
 exports.down = function(knex) {
-  return knex.schema.alterTable('units', (table) => {
-    // TODO: This migration does not work properly
-    table
-      .timestamp('deleted_at')
-      .notNull()
-      .defaultTo('2020-01-01 00:00:00')
-      .alter();
-  });
+  // Bypass down migration.
+  // If we set .notNullable() we will need to also set default
+  // value which I can't get to work. Don't think it is worth
+  // bothering with getting the table set back to .notNullable
+  // on migrate down as it is unlikely to cause any problems.
+  return knex;
 };
