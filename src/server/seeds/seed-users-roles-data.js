0683bdf2670cfec91803858433a46227d9254b9e
Zaki Wasik <zakiwa@gmail.com>
2020-04-06 23:04:33 +0200

    refactor: reformat with prettier

--- src/server/seeds/seed-users-roles-data.js
+++ src/server/seeds/seed-users-roles-data.js
@@ -1,7 +1,7 @@
-exports.seed = function (knex) {
+exports.seed = function(knex) {
   return knex('user_roles')
     .del()
-    .then(function () {
+    .then(function() {
       return knex('user_roles').insert([
         { fk_user_id: 1, fk_role_id: 1 },
         { fk_user_id: 2, fk_role_id: 2 },
