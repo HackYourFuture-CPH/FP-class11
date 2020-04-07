09c4c13ba3a869631eeb1d88422923151b01d182
mahie akhtar <mahie.akhtar@outlook.com>
2020-04-07 15:13:05 +0200

    migrations for sensors_readings and crop_stage_default_values schemas

---
+++ src/server/migrations/20200407123155_create_sensors_readings.js
@@ -0,0 +1,40 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('sensors_readings', (table) => {
+    table
+      .increments('id')
+      .primary()
+      .notNullable();
+    table
+      .integer('fk_batch_id')
+      .notNullable()
+      .unsigned();
+    table
+      .integer('fk_material_id')
+      .notNullable()
+      .unsigned();
+    table.float('reading_value').notNullable();
+    table
+      .timestamp('created_at')
+      .defaultTo(knex.fn.now())
+      .notNullable();
+    table
+      .timestamp('updated_at')
+      .defaultTo(knex.fn.now())
+      .notNullable();
+    table
+      .timestamp('deleted_at')
+      .defaultTo(null)
+      .nullable();
+    table
+      .foreign('fk_batch_id')
+      .references('id')
+      .inTable('batches');
+    table
+      .foreign('fk_material_id')
+      .references('id')
+      .inTable('materials');
+  });
+};
+exports.down = function(knex) {
+  return knex.schema.dropTable('sensors_readings');
+};
