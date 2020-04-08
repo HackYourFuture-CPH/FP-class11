cf33e601915206886c3eecc2ee95bd8a5a77d2d9
nazaqat ali <naa.dk@yahoo.com>
2020-04-08 15:13:34 +0200

    knex renamed to knexfile.js

---
+++ src/server/knexfile.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+require('dotenv').config({ path: '../../.env' });
+
+module.exports = {
+  development: {
+    client: 'mysql2',
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    },
+    pool: { min: 0, max: 7 },
+    seeds: {
+      directory: path.join(__dirname, '/seeds'),
+    },
+  },
+};
