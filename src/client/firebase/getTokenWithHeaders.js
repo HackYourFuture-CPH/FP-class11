fbd36610a2eaae4fa1c644980a4492bff39accd3
Sowmya Mannem <sowmyamannemr@gmail.com>
2020-04-09 12:57:43 +0200

    Firebase middlewares usage/Checks and other changes related to token (#141)
    * added a md file for the checkpoints using middleware and added that to README file
    
    * removed the text Sowmya in authorization key value
    
    * removed the duplicate admin folder and moved the my sql operation in to a function
    
    * refracted the code based on feedback
    
    * use checkIfAuthenticated middleware if endpoint requires no additional user claims
    
    * append word Bearer to request header
    
    Co-authored-by: nig <nig@beepanalytics.com>

--- src/client/firebase/getTokenWithHeaders.js
+++ src/client/firebase/getTokenWithHeaders.js
@@ -4,7 +4,7 @@ export const getTokenWithHeaders = async () => {
   const token = await Firebase.getAuth().currentUser.getIdToken();
   const headers = {
     'Content-Type': 'application/json',
-    authorization: `Sowmya ${token}`,
+    authorization: `Bearer ${token}`,
   };
   return headers;
 };
