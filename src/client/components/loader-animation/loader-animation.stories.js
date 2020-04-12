4f0f8049a291a995bb0957ba5e8737d73fef2470
humayunadilshahzad <35384625+humayunadilshahzad@users.noreply.github.com>
2020-04-12 19:17:47 +0200

    Feature/loader component (#153)
    * loader animation and storybook added
    
    * Revert "packages updated"
    
    This reverts commit 0184a40c7fabd951c94c5f789302f032d0f0f09d.

---
+++ src/client/components/loader-animation/loader-animation.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import LoaderAnimation from './loader-animation.component';
+
+export default {
+  title: 'Loader Animation',
+  component: LoaderAnimation,
+};
+
+export const Loader = () => <LoaderAnimation />;
