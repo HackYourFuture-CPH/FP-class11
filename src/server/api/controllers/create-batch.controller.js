057b8e576012d7b6d0bb9c9e7b846972449dcaee
Gizem Candemir <gizemc@gmail.com>
2020-04-14 12:30:10 +0200

    Add controller and route for create-batch

--- src/server/api/controllers/create-batch.controller.js
+++ src/server/api/controllers/create-batch.controller.js
@@ -1,11 +1,23 @@
 const knex = require('../../config/db');
 const Error = require('../lib/utils/http-error');
 
-const createBatch = async (cropId, userId) => {
+const createBatch = async (
+  cropId,
+  userId,
+  customerName,
+  seedPot,
+  startSeedDate,
+) => {
   try {
     const batch = await knex('batches').insert({
       fk_crop_id: cropId,
       fk_user_id: userId,
+      customer_name: customerName,
+      number_of_seeded_pots: seedPot,
+      seeding_date: startSeedDate,
+      created_at: knex.fn.now(),
+      updated_at: knex.fn.now(),
+      deleted_at: '',
     });
     if (cropId <= 0) {
       throw new Error(`incorrect entry with the id of ${cropId}`, 404);
--- src/server/api/routes/create-batch.router.js
+++ src/server/api/routes/create-batch.router.js
@@ -7,9 +7,15 @@ const router = express.Router({ mergeParams: true });
 const createBatchController = require('../controllers/create-batch.controller');
 
 // ENDPOINT: /api/modules/:cropId :GET to get one crop's default values
-router.get('/:cropId', (req, res, next) => {
+router.post('/create-batch', (req, res, next) => {
   createBatchController
-    .createBatch(req.params.cropId, req.params.userId)
+    .createBatch(
+      req.params.cropId,
+      req.params.userId,
+      req.params.customerName,
+      req.params.seedPot,
+      req.params.startSeedDate,
+    )
     .then((result) => res.json(result))
     .catch(next);
 });
