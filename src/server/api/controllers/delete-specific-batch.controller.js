afbe053d2109a008826f7b60010310d181109ea6
hem@nth kum@r <52456390+hemanth06912@users.noreply.github.com>
2020-04-14 06:45:04 +0200

    updated changed batches_table to batches

--- src/server/api/controllers/delete-specific-batch.controller.js
+++ src/server/api/controllers/delete-specific-batch.controller.js
@@ -2,12 +2,12 @@ const knex = require('../../config/db');
 
 const deleteSpecificBatchById = async (batchId) => {
   try {
-    await knex('batches_table')
+    await knex('batches')
       .where({ id: batchId })
       .del()
       .forUpdate()
       .insert([{ deleted_at: new Date().toLocaleString() }], ['batchId'])
-      .into('batches_table');
+      .into('batches');
     return 'the batch is deleted';
   } catch (error) {
     return error.message;
