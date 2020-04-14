35f30ed8e0d5e6c5dc7a41163321edc6cb994170
Gizem Candemir <gizemc@gmail.com>
2020-04-14 13:28:54 +0200

    Add 'checkIfAuthenticated' to create-batch route

--- src/server/api/routes/create-batch.router.js
+++ src/server/api/routes/create-batch.router.js
@@ -7,7 +7,7 @@ const router = express.Router({ mergeParams: true });
 const createBatchController = require('../controllers/create-batch.controller');
 
 // ENDPOINT: /api/modules/:cropId :GET to get one crop's default values
-router.post('/create-batch', (req, res, next) => {
+router.post('/create-batch', 'checkIfAuthenticated', (req, res, next) => {
   createBatchController
     .createBatch(
       req.params.cropId,
