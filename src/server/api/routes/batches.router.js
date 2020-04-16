106690fe6c3774707b641e2a4a312ee1ef3d1d86
Kristina Fazinić <kfazinic@outlook.com>
2020-04-16 12:13:33 +0200

    Add authentication middleware to routes

--- src/server/api/routes/batches.router.js
+++ src/server/api/routes/batches.router.js
@@ -3,11 +3,15 @@ const express = require('express');
 
 const router = express.Router({ mergeParams: true });
 
+const {
+  checkIfAuthenticated,
+} = require('../lib/middleware/authentication.middleware');
+
 // controllers
 const batchController = require('../controllers/batches.controller');
 
 // ENDPOINT: /api/batch/:id :GET to get one module
-router.get('/:batchId', (req, res, next) => {
+router.get('/:batchId', checkIfAuthenticated, (req, res, next) => {
   batchController
     .getBatchById(req.params.batchId)
     .then((result) => res.json(result))
--- src/server/api/routes/create-batch.router.js
+++ src/server/api/routes/create-batch.router.js
@@ -3,11 +3,15 @@ const express = require('express');
 
 const router = express.Router({ mergeParams: true });
 
+const {
+  checkIfAuthenticated,
+} = require('../lib/middleware/authentication.middleware');
+
 // controllers
 const createBatchController = require('../controllers/create-batch.controller');
 
 // ENDPOINT: /api/modules/:cropId :GET to get one crop's default values
-router.get('/:cropId', (req, res, next) => {
+router.get('/:cropId', checkIfAuthenticated, (req, res, next) => {
   createBatchController
     .createBatch(req.params.cropId, req.params.userId)
     .then((result) => res.json(result))
