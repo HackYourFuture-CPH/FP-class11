2ae21c25bb5d45a1ef85d5246c8897c8f221dc54
Kristina Fazinić <kfazinic@outlook.com>
2020-04-17 15:32:35 +0200

    Rename new env vars

--- src/client/helpers/signInAsDefaultUser.js
+++ src/client/helpers/signInAsDefaultUser.js
@@ -2,7 +2,7 @@ import Firebase from '../firebase/index';
 
 export default async function signInAsDefaultUser() {
   await Firebase.signInEmailAndPassword(
-    process.env.REACT_APP_DEFAULT_USER,
-    process.env.REACT_APP_DEFAULT_PASSWORD,
+    process.env.DEFAULT_USER,
+    process.env.DEFAULT_PASSWORD,
   );
 }
