14506acc83c86d6945d582477d757b02d7d8b795
Ekamba <axelgassay@yahoo.dk>
2020-04-20 18:07:53 +0200

    refactoring all components to solv UI view

--- src/client/components/delete-confim/modal-list.component.js
+++ src/client/components/delete-confim/modal-list.component.js
@@ -14,33 +14,31 @@ const ModalList = ({ setIsShowing, onDelete }) => {
         tabIndex={-1}
         role="dialog"
       >
-        <div className="modal-header">
-          <span
-            type="button"
-            className="modal-close-button"
-            aria-label="Close"
+        <span
+          type="button"
+          className="modal-close-button"
+          aria-label="Close"
+          onClick={() => setIsShowing(false)}
+          aria-hidden="true"
+        >
+          &times;
+        </span>
+
+        <h4 className="text-headline">DELETE BATCH</h4>
+        <p className="p1">Are you sure?</p>
+        <p className="p2">Do you really want to delete the batch?</p>
+        <div className="buttons-wrapper">
+          <Button type="danger" size="large" onClick={onDelete}>
+            Delete
+          </Button>
+          <br />
+          <Button
+            type="secondary"
+            size="large"
             onClick={() => setIsShowing(false)}
-            aria-hidden="true"
           >
-            &times;
-          </span>
-        </div>
-        <div className="modal">
-          <h4 className="text-headline">DELETE CROP</h4>
-          <p className="p1">Are you sure?</p>
-          <p className="p2">Do you really want to delete the crop?</p>
-          <div className="buttons-wrapper">
-            <Button type="danger" size="large" onClick={onDelete}>
-              Delete
-            </Button>
-            <Button
-              type="secondary"
-              size="large"
-              onClick={() => setIsShowing(false)}
-            >
-              Cancel
-            </Button>
-          </div>
+            Cancel
+          </Button>
         </div>
       </div>
     </div>,
