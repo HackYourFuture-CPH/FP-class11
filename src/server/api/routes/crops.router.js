d07af93841d334c244e78616f16dae4ccebb192a
ricardo aguiar <barrios.dev@gmail.com>
2020-04-20 13:40:17 +0200

    added swagger auto gen docs to endpoint

--- src/server/api/routes/crops.router.js
+++ src/server/api/routes/crops.router.js
@@ -10,6 +10,34 @@ const {
   checkIfAuthenticated,
 } = require('../lib/middleware/authentication.middleware');
 
+/**
+ * @swagger
+ * /crops/:
+ *   get:
+ *     summary: Get all crops
+ *     description:
+ *       Get all crops
+ *     tags:
+ *       - Crops
+ *     operationId: getAllCrops
+ *     produces: application/json
+ *     parameters:
+ *      - name: authorization
+ *        in: header
+ *        description: Firebase token
+ *        required: true
+ *        default: Bearer ENTER_FIREBASE_TOKEN
+ *        type: string
+ *     responses:
+ *       200:
+ *         description: Successful request
+ *       401:
+ *         description: You are not authorized to view this content.
+ *       404:
+ *         description: A batch with the specified id was not found.
+ *       5XX:
+ *         description: Unexpected error.
+ */
 // ENDPOINT: /api/crops/ :GET to get all crops
 router.get('/', checkIfAuthenticated, (req, res, next) => {
   cropsController
