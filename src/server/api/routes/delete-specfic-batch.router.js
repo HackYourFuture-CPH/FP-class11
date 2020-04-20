34f5ca27ea5cc3f86378e574d0c40427b206648b
ricardo aguiar <barrios.dev@gmail.com>
2020-04-20 21:27:36 +0200

    added delete api endpoint to swagger

--- src/server/api/routes/delete-specfic-batch.router.js
+++ src/server/api/routes/delete-specfic-batch.router.js
@@ -10,6 +10,44 @@ const router = express.Router({ mergeParams: true });
 // controllers
 const deleteSpecificDataController = require('../controllers/delete-specific-batch.controller');
 
+/**
+ * @swagger
+ * /delete-specific-data/{batchId}:
+ *   delete:
+ *     summary: Delete a batch by id number
+ *     description:
+ *       Delete a batch by batch id number
+ *
+ *
+ *        Authentication&#58; <code>super_admin</code> only.
+ *     tags:
+ *       - Batches
+ *     operationId: deleteSpecificBatchById
+ *     produces: application/json
+ *     parameters:
+ *      - name: authorization
+ *        in: header
+ *        description: Firebase token
+ *        required: true
+ *        default: Bearer ENTER_FIREBASE_TOKEN
+ *        type: string
+ *      - name: batchId
+ *        in: path
+ *        type: integer
+ *        required: true
+ *     security:
+ *         firebase_auth:
+ *         - read
+ *     responses:
+ *       200:
+ *         description: Successful request
+ *       401:
+ *         description: Unauthorized request.
+ *       404:
+ *         description: Not found.
+ *       5XX:
+ *         description: Unexpected error.
+ */
 // ENDPOINT: /api/example/ :DELETE
 router.delete('/:batchid', checkIfAuthorized(Roles.SUPER_ADMIN), (req, res) => {
   deleteSpecificDataController
