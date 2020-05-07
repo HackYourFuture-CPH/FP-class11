dd07dfd9ae3bfb10292169b79afb1129f8cce515
ricardo aguiar <barrios.dev@gmail.com>
2020-05-07 20:35:58 +0200

    moved swagger definition to swagger-config file

--- src/server/api/routes/api-router.js
+++ src/server/api/routes/api-router.js
@@ -1,6 +1,11 @@
 const express = require('express');
 
 const router = express.Router();
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const swaggerOptions = require('../../config/swagger-config');
+
+const swaggerDocument = swaggerJsDoc(swaggerOptions);
 
 // Router imports
 const modulesRouter = require('./modules.router');
@@ -30,38 +35,7 @@ router.use('/batch-default-values', batchDefaultValuesRouter);
 router.use('/batch-status/', batchStatusRouter);
 router.use('/delete-specific-batch', deleteSpecificBatchDataRouter);
 
-// swagger-ui-express
-const swaggerJsDoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
-
-const swaggerOptions = {
-  swaggerDefinition: {
-    info: {
-      version: '1.0',
-      title: 'Final project',
-      description: 'API documentation for the final project',
-      contact: {},
-    },
-    host: '',
-    basePath: '/api',
-  },
-  securityDefinitions: {
-    firebase_auth: {
-      type: 'oauth2',
-      authorizationUrl: 'seasony-login.firebaseapp.com',
-      description: 'This API uses OAuth 2.',
-      flows: 'authorizationCode',
-      scopes: {
-        read: 'read data based on user uid',
-        write: 'write data based on user uid',
-      },
-    },
-  },
-  apis: ['./src/server/api/routes/*.js'],
-};
-
 // Route for Swagger API Documentation
-const swaggerDocument = swaggerJsDoc(swaggerOptions);
 router.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
