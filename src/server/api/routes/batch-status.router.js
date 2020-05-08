684d79fe8a1ad0fbd48ffc3540a4950fe4d82d05
ricardo aguiar <barrios.dev@gmail.com>
2020-05-08 15:47:27 +0200

    implemented documentation for batch-status endpoint

--- src/server/api/routes/batch-status.router.js
+++ src/server/api/routes/batch-status.router.js
@@ -10,6 +10,42 @@ const {
 // controllers
 const batchStatusController = require('../controllers/batch-status.controller');
 
+/**
+ * @swagger
+ * /batch-status/{batchId}:
+ *   get:
+ *     summary: Get batch status for specific batch
+ *     description:
+ *       Get batch status for specific batch by batchId<br /><br />
+ *       Authorization&#58;  <code>user</code>
+ *     tags:
+ *       - Batches
+ *     operationId: getBatchStatus
+ *     produces: application/json
+ *     parameters:
+ *      - name: authorization
+ *        in: header
+ *        description: Firebase token
+ *        required: true
+ *        default: Bearer ENTER_FIREBASE_TOKEN
+ *        type: string
+ *      - name: batchId
+ *        in: path
+ *        type: integer
+ *        required: true
+ *     security:
+ *        fireabse_auth:
+ *        - read
+ *     responses:
+ *       200:
+ *         description: Successful request
+ *       401:
+ *         description: You are not authorized to view this content.
+ *       404:
+ *         description: Not found.
+ *       5XX:
+ *         description: Unexpected error.
+ */
 // ENDPOINT: /api/batch-status/:batchId :GET to get status messages for a batch with batchId
 router.get('/:batchId', checkIfAuthenticated, (req, res, next) => {
   batchStatusController
